67642f9d299a22c0c9001d245a54a8d8dcbfe92d
Mgalvezgo <141689220+Mgalvezgo@users.noreply.github.com>
2023-09-04 19:15:43 -0600

    Add files via upload

---
+++ Semana No.05 Laboratorio/L1MG1100823/L1MG1100823/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1MG1100823
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo soy Marcela");
+            Console.ReadKey();
+
+            Console.WriteLine(" Hola mundo");
+            Console.WriteLine(" Soy Marcela");
+
+            /* Comentaarios 
+            Console.WriteLine("Hola mundo soy Marcela");
+            Console.ReadKey();
+            Console.WriteLine(" Hola mundo");
+            Console.WriteLine(" Soy Marcela");*/
+
+            Console.Write("Hola mundo");
+            Console.Write("Soy Marcela");
+            Console.ReadKey();
+
+            Console.WriteLine("Marcela");
+            string Nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("soy" + Nombre);
+
+            /*Comentario*/
+
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("soy" + Nombre);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Semana No.05 Laboratorio/T1MG1100823/T1MG1100823/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1MG1100823
+{
+
+class Program
+        {
+            static void Main()
+            {
+                // Variables con String
+                string sNombre, sEdad, sCarrera, sCarné;
+
+                // Solicitar al usuario que ingrese los datos
+                Console.WriteLine("Mi segundo programa");
+                Console.Write("Nombre: ");
+                sNombre = Console.ReadLine();
+                Console.Write("Edad: ");
+                sEdad = Console.ReadLine();
+                Console.Write("Carrera: ");
+                sCarrera = Console.ReadLine();
+                Console.Write("Carné: ");
+                sCarné = Console.ReadLine();
+
+                // Mostrar los datos ingresados por el usuario
+                Console.WriteLine("Mi segundo programa");
+                Console.WriteLine("Nombre: " + sNombre);
+                Console.WriteLine("Edad: " + sEdad);
+                Console.WriteLine("Carrera: " + sCarrera);
+                Console.WriteLine("Carné: " + sCarné);
+
+                // Mensaje adicional
+                Console.WriteLine("Soy " + sNombre + ", tengo " + sEdad + " años y estudio la carrera de " + sCarrera + ".");
+                Console.WriteLine("Mi número de carné es: " + sCarné);
+
+                // Pausa
+                Console.ReadKey();
+            }
+        }
+
+
+
+
+    }
+
