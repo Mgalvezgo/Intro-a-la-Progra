0e40a8c6bec4bdeb4336e3ffc867f56d886f5deb
Mgalvezgo <141689220+Mgalvezgo@users.noreply.github.com>
2023-09-18 21:26:21 -0600

    Add files via upload

---
+++ SEMANA_7_LABORATORIO_1100823_MRCELA_GÀLVEZ/SEMANA_7_TAREA_MARCELA GÁLVEZ_1100823/T4_MG_1100823/T4_MG_1100823/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_MG_1100823
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Laboratorio No. 4");
+            Console.WriteLine("Ejercicio No. 1");
+
+            // Declaración de variables
+            double V0, tiempo, aceleracion;
+
+            // Solicitar y leer valores de entrada
+            Console.Write("Ingrese la velocidad inicial (V0): ");
+            V0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el tiempo (t): ");
+            tiempo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la aceleración (a): ");
+            aceleracion = Convert.ToDouble(Console.ReadLine());
+
+            // Verificar que los valores sean válidos
+            if (V0 >= 0 && tiempo >= 0 && aceleracion >= 0)
+            {
+                // Calcular la velocidad final (Vf)
+                double Vf = V0 + (aceleracion * tiempo);
+
+                // Mostrar el resultado
+                Console.WriteLine("La velocidad final (Vf) es: " + Vf);
+            }
+            else
+            {
+                Console.WriteLine("Los valores ingresados deben ser mayores o iguales a cero.");
+            }
+
+            Console.ReadLine(); // Para no cerrar el Programa
+
+        }
+        }
+}
+
+
+
+
